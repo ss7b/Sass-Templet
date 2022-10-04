3c2a789dd4260c8d88888f60a3f23fa566f704ee
suhail <babks93@gmial.com>
2022-10-04 18:57:09 +0300

    to top btn

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
 document.getElementById("year").innerHTML = new Date().getFullYear();
+
+let form = document.getElementById("form")
+let nameInput = document.getElementById("name");
+let emailInput = document.getElementById("email");
+
+form.onsubmit = function(e){
+    validName = false;
+    validEmail = false;
+    
+    if(nameInput.value !== ""){
+        validName = true;
+    };
+    if(emailInput.value !== ""){
+        validName = true;
+    }
+    if(validName === false || validEmail === false){
+        e.preventDefault();
+    };
+}
+
+let toTop = document.getElementById("to-top");
+console.log(toTop)
+// 
+
+window.onscroll = function(){
+    if(scrollY >= 799){
+        console.log(`${window.scrollY}`)
+        toTop.style.opacity = 1;
+    }else{
+        toTop.style.opacity = 0;
+    }
+}
+toTop.addEventListener("click",function(){
+    window.scrollTo({
+        top:0,
+        left:0,
+        behavior : "smooth",
+    })
+})
